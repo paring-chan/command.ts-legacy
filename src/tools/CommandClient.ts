77bced7bca33ebaeec49601b1eec9d64f1b25be1
pikokr <parangee9706@gmail.com>
2021-01-22 20:47:10 +0900

    feat: init typescript project and basic bot

---
+++ src/tools/CommandClient.ts
@@ -0,0 +1,13 @@
+import { Client, ClientOptions, Util } from "discord.js";
+import { CommandClientOptions } from "../types";
+
+export default class CommandClient extends Client {
+    constructor(options?: CommandClientOptions={}, clientOptions?: ClientOptions) {
+        super(clientOptions)
+        Util.mergeDefault({
+            commandHandler: {
+                watch: false
+            }
+        }, options)
+    }
+}
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface CommandClientOptions {
+    commandHandler?: {
+        watch?: boolean
+    }
+}
