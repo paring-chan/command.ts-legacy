4c10182df8ceaa2061a39c0eea67886ba347bf3d
pikokr <parangee9706@gmail.com>
2021-01-22 20:47:32 +0900

    fix: some bugs

--- src/tools/CommandClient.ts
+++ src/tools/CommandClient.ts
@@ -2,7 +2,7 @@ import { Client, ClientOptions, Util } from "discord.js";
 import { CommandClientOptions } from "../types";
 
 export default class CommandClient extends Client {
-    constructor(options?: CommandClientOptions={}, clientOptions?: ClientOptions) {
+    constructor(options: CommandClientOptions={}, clientOptions?: ClientOptions) {
         super(clientOptions)
         Util.mergeDefault({
             commandHandler: {
